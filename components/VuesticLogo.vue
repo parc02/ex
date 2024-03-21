5f8ecb1575c0cf22d5c7a07f569c49d1444b2e16
parc02 <pascalmin49@gmail.com>
2024-03-21 20:50:03 +0900

    first commit

---
+++ components/VuesticLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <img src="https://i.ibb.co/JkvZCvh/2024-03-08-144708.png" height="200" width="200" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useColors } from 'vuestic-ui'
+</script>
